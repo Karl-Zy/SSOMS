8cc1601864aaed121ae80b7c4a622c6d96627f91
Karl-Zy <karl.chiong1@gmail.com>
2025-05-06 15:05:19 +0800

    LoginController

---
+++ src/ssoms/LoginController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ssoms;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Karlllllllllllllllll
+ */
+public class LoginController {
+
+    @FXML
+    private TextField usernameLogin;
+    @FXML
+    private PasswordField passwordLogin;
+
+    @FXML
+    private void lgnButtonOnAction(ActionEvent event) {
+    }
+    
+}
