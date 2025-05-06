934e5561c4f5d0d62b44b4f0685b63f789fbebdd
rohan <rohan@10.121.2.154>
2025-05-06 15:28:59 +0800

    balh

--- src/ssoms/LoginController.java
+++ src/ssoms/LoginController.java
@@ -4,11 +4,18 @@
  */
 package ssoms;
 
+import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  *
@@ -22,7 +29,31 @@ public class LoginController {
     private PasswordField passwordLogin;
 
     @FXML
-    private void lgnButtonOnAction(ActionEvent event) {
+    private void lgnButtonOnAction(ActionEvent event) throws IOException {
+         String usern = usernameLogin.getText();
+        String passw = passwordLogin.getText();
+     
+
+        if (usern.equalsIgnoreCase("admin") && passw.equals("123")) {
+
+            Parent root = FXMLLoader.load(getClass().getResource("HomepageForm.fxml"));
+
+            Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            primaryStage.setScene(new Scene(root));
+            primaryStage.centerOnScreen();
+            primaryStage.setTitle("Verdant's Kitchen");
+            primaryStage.show();
+            primaryStage.setResizable(false);
+
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Failed");
+            alert.setHeaderText(null);
+            alert.setContentText("Invalid username or password.");
+            alert.showAndWait();
+        }
+    }
+        
     }
     
-}
+
